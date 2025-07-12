11270377a54eabaa85c3eab027a3b1ee0f58276a
Abhishek <abhi042004@outlook.com>
2025-07-12 23:13:43 +0530

    Merge branch 'main' of https://github.com/Abhi042004/basic-java-programs

--- 02/dollar_converter.java
+++ 02/dollar_converter.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class dollar_converter {
     public static void main(String[] args) {
         
+    Scanner sc=new Scanner(System.in);
+
+    System.out.println("Enter the rupee amount: ");
+    float rupee= sc.nextFloat();
+
+    double dollar=rupee/85.8;
+
+    System.out.println("Dollar: $"+Math.round(dollar*100)/100.0);
+
+    sc.close();
+
     }
 }
